a6408e5cc59be68b9c43fc1b91834deae5252cff
mark <102784820+comet1019@users.noreply.github.com>
2023-11-06 21:06:40 +0900

    다시가자

--- app.js
+++ app.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/hello',(req,res)=>{
-    res.json({title:"wow..", name:"comet", age: 10})
-})
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
